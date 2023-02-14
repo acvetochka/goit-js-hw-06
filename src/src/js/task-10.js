a64f8a1d1ea47f28bcf19d9defe55e76f74ac742
Alona Kuznietsova <acvetochka@gmail.com>
2023-02-14 09:46:49 +0100

    add task-10

---
+++ src/src/js/task-10.js
@@ -0,0 +1,33 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const input = document.querySelector('input');
+const buttonCreate = document.querySelector('[data-create]');
+const buttonDestroy = document.querySelector('[data-destroy]');
+
+const divBoxes = document.querySelector('#boxes');
+
+function createBoxes(amount) {
+  amount = input.value;
+  for (let i = 0; i < amount; i += 1) {
+    const div = document.createElement("div");
+    const size = 30 + 10*i;
+    divBoxes.append(div);
+    div.style.width = `${size}px`;
+    div.style.height = `${size}px`;
+    div.style.backgroundColor = getRandomHexColor();
+  }
+}
+
+buttonCreate.addEventListener("click", createBoxes);
+
+function destroyBoxes() {
+  divBoxes.textContent = "";
+  // const divs = document.querySelectorAll('#boxes div');
+  // divs.forEach((div) => {
+  //   div.remove();
+  // })
+}
+
+buttonDestroy.addEventListener("click", destroyBoxes);
