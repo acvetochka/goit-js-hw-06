7ae4c74f808d5d1993471d286d2469b7979b4272
Alona Kuznietsova <acvetochka@gmail.com>
2023-04-03 22:31:58 +0200

    add default fontsize

--- js/task-07.js
+++ js/task-07.js
@@ -1,5 +1,6 @@
 const input = document.querySelector('#font-size-control');
 const span = document.querySelector('#text');
+span.style.fontSize = `${input.value}px`;
 
 input.addEventListener("input", event => {
     span.style.fontSize = `${event.target.value}px`;
